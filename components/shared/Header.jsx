61a33edbae06d6b23205de1bbe3f21a80ff53287
Justin Morgan <justin.morgan@hey.com>
2021-09-17 14:17:00 -0700

    :sparkles: (Header) Implement shared header with current link highlighting

---
+++ components/shared/Header.jsx
@@ -0,0 +1,106 @@
+import styled from 'styled-components'
+import Logo from '../svg/Logo'
+import ThemeSwitcher from '../svg/ThemeSwitcher'
+import { OutlineButton, RoundButton, SolidButton } from '../ui/Button'
+import NextLink from 'next/link'
+import { useRouter } from 'next/router'
+
+const Header = styled.header`
+  display: flex;
+  justify-content: center;
+  padding: 1.5rem 0;
+`
+
+const Nav = styled.nav`
+  display: flex;
+  flex-grow: 1;
+  justify-content: space-between;
+  align-items: center;
+  max-width: ${({ theme }) => theme.width.max_desktop};
+
+  & > ul {
+    list-style: none;
+    display: flex;
+    gap: 1.4rem;
+  }
+`
+
+const BrandName = styled.h1`
+  text-decoration: none;
+  color: ${({ theme }) => theme.colors.black};
+`
+
+const Link = styled.a`
+  color: ${({ theme }) => theme.colors.grey_600};
+`
+const CurrentLink = styled.a`
+  color: ${({ theme }) => theme.colors.grey_600};
+  font-weight: bold;
+`
+
+const BrandLink = styled.a`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`
+
+const routes = [
+  ['/', 'Product'],
+  ['/customers', 'Customers'],
+  ['/pricing', 'Pricing'],
+  ['/resources', 'Resources'],
+]
+
+const Component = () => {
+  const router = useRouter()
+
+  return (
+    <Header>
+      <Nav>
+        <BrandLink href="/">
+          <Logo />
+          <BrandName>Product</BrandName>
+        </BrandLink>
+        <ul>
+          {routes.map(([href, text]) => (
+            <MaybeCurrentRoute
+              key={href}
+              currentPath={router.asPath}
+              href={href}
+              text={text}
+            />
+          ))}
+          <li>
+            <NextLink href="/sign-in">
+              <OutlineButton as="a">Sign In</OutlineButton>
+            </NextLink>
+          </li>
+          <li>
+            <NextLink href="/sign-up">
+              <SolidButton as="a">Sign Up</SolidButton>
+            </NextLink>
+          </li>
+          <li>
+            <RoundButton aria-label="Toggle Theme">
+              <ThemeSwitcher />
+            </RoundButton>
+          </li>
+        </ul>
+      </Nav>
+    </Header>
+  )
+}
+
+const MaybeCurrentRoute = (props) => {
+  const { currentPath, href, text } = props
+  const isCurrent = href == currentPath
+  return (
+    <li key={href}>
+      <NextLink {...props} passHref>
+        {isCurrent ? <CurrentLink>{text}</CurrentLink> : <Link>{text}</Link>}
+      </NextLink>
+    </li>
+  )
+}
+
+export default Component
--- theme.js
+++ theme.js
@@ -2,16 +2,30 @@ import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
 
-
     body {
         margin: 0;
         padding: 0;
         box-sizing: border-box;
         font-family: Poppins, Futura, 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
     }
+
+    *, *::before, *::after {
+      box-sizing: border-box;
+    }
+
+    a {
+      text-decoration: none;
+    }
+
+    button {
+      border: none;
+    }
 `
 
 export const theme = {
+  width: {
+    max_desktop: '62.5rem',
+  },
   colors: {
     primary: '#02897A',
     green: '#02897A',
